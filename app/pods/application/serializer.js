4ac2ea98afc43c4739f4df5478827b4dd8b8ed7b
Richard <hello@noxan.net>
2016-08-08 16:05:15 +0200

    override modelNameFromPayloadKey in application serializer to transform containers to docker-containers

--- app/pods/application/serializer.js
+++ app/pods/application/serializer.js
@@ -1,4 +1,13 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
+  modelNameFromPayloadKey(payloadKey) {
+    let key = payloadKey;
+
+    if (payloadKey === 'containers') {
+      key = `docker-${payloadKey}`;
+    }
+
+    return this._super(key);
+  },
 });
