6d70d14159d40b89865edb46140e7b215812ae9a
Richard <hello@noxan.net>
2016-08-08 16:14:07 +0200

    define fields of docker container model

--- app/models/docker-container.js
+++ app/models/docker-container.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  name: DS.attr(),
+  // image: DS.belongsTo('docker-image'),
+  status: DS.attr(),
+  created: DS.attr('date'),
+  command: DS.attr(),
+  ports: DS.attr(),
 });
