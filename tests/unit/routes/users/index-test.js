a77283686ea38117cd693a1fcfd48a50e6d8f569
Richard <hello@noxan.net>
2016-08-08 13:26:34 +0200

    autofix eslint warnings in tests folder

--- tests/unit/routes/users/index-test.js
+++ tests/unit/routes/users/index-test.js
@@ -5,7 +5,7 @@ moduleFor('route:users/index', 'Unit | Route | users/index', {
   // needs: ['controller:foo']
 });
 
-test('it exists', function(assert) {
+test('it exists', function (assert) {
   let route = this.subject();
   assert.ok(route);
 });
