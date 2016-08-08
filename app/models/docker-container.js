eb38ce11b9cc875dbdd1176535afab8097b2cd04
Richard <hello@noxan.net>
2016-08-08 18:11:09 +0200

    enable image relation of container

--- app/models/docker-container.js
+++ app/models/docker-container.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr(),
-  // image: DS.belongsTo('docker-image'),
+  image: DS.belongsTo('docker-image'),
   status: DS.attr(),
   created: DS.attr('date'),
   command: DS.attr(),
