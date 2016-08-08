b734a64daee1d32aefbd56564446df6299acdef6
Richard <hello@noxan.net>
2016-08-08 11:40:50 +0200

    add action handler to login form component

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  email: '',
+  password: '',
+
+  actions: {
+    authenticate() {
+      const props = this.getProperties('email', 'password');
+      console.log(props);
+    },
+  },
 });
