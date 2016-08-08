e0b99c2eb262eab46afbc1d44b9c95c76d3b2347
Richard <hello@noxan.net>
2016-08-08 15:28:00 +0200

    reset changes to user model test

--- tests/unit/models/user-test.js
+++ tests/unit/models/user-test.js
@@ -2,11 +2,11 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('user', 'Unit | Model | user', {
   // Specify the other units that are required for this test.
-  needs: [],
+  needs: []
 });
 
-test('it exists', assert => {
-  const model = this.subject();
+test('it exists', function(assert) {
+  let model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
 });
