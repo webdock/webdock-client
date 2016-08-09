12985a3317ddec50810c5c2f4b9944684d589b92
Andre Kupka <kupka@in.tum.de>
2016-08-09 11:42:04 +0200

    add language auto detection

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -1,10 +1,33 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import config from '../../config/environment';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
   i18n: Ember.inject.service(),
 
   beforeModel() {
-    this.set('i18n.locale', 'en');
+    const locale = this.determineLocale();
+    this.set('i18n.locale', locale);
+  },
+
+  determineLocale() {
+    const defaultLocale = config.i18n.defaultLocale;
+    const locales = this.get('i18n.locales');
+    const locale = navigator.languages ? navigator.languages[0] : navigator.language || navigator.userLanguage || defaultLocale;
+
+    if (locales.contains(locale)) {
+      return locale;
+    }
+
+    let matchingLocale = null;
+    locales.forEach(elem => {
+      if (locale.startsWith(elem)) {
+        if (matchingLocale === null || elem.length > matchingLocale.length) {
+          matchingLocale = elem;
+        }
+      }
+    });
+
+    return matchingLocale || defaultLocale;
   },
 });
