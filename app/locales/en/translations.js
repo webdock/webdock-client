e8d6d3436706562bf4b23956f4d4dd275a0c9ce9
Andre Kupka <kupka@in.tum.de>
2016-08-09 11:17:02 +0200

    add en locale for components

---
+++ app/locales/en/translations.js
@@ -0,0 +1,46 @@
+export default {
+  'container-start-stop-button': {
+    'start': 'Start',
+    'stop': 'Stop',
+  },
+  'docker-container-detail': {
+    'name': 'Name',
+    'status': 'Status',
+    'command': 'Command',
+    'id': 'Id',
+    'image': 'Image',
+  },
+  'docker-container-list': {
+    'id': 'Id',
+    'name': 'Name',
+    'status': 'Status',
+    'command': 'Command',
+    'image': 'Image',
+  },
+  'docker-image-detail': {
+    'repository': 'Repository',
+    'created': 'Created',
+    'size': 'Size',
+    'virtual-size': 'Virtual Size',
+    'parent-id': 'Parent Id',
+    'id': 'Id',
+  },
+  'docker-image-list': {
+    'id': 'Id',
+    'repository': 'Repository',
+    'created': 'Created',
+    'size': 'Size',
+  },
+  'login-form': {
+    'email': 'Email',
+    'password': 'Password',
+    'login': 'Login',
+  },
+  'nav-bar': {
+    'home': 'Home',
+    'containers': 'Containers',
+    'images': 'Images',
+    'users': 'Users',
+    'logout': 'Logout',
+  }
+};
