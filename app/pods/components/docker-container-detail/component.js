8b422bb04fe77b78976cbcfe1ab6e1bfcdf10219
Richard <hello@noxan.net>
2016-08-09 12:44:53 +0200

    define remove action for docker container in detail component

--- app/pods/components/docker-container-detail/component.js
+++ app/pods/components/docker-container-detail/component.js
@@ -2,4 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   dockerContainer: null,
+
+  actions: {
+    remove() {
+      this.get('dockerContainer').destroyRecord();
+    },
+  },
 });
