155c02c5f5e37b4ffe584f0945257585026444e4
Andre Kupka <kupka@in.tum.de>
2016-08-09 11:02:01 +0200

    set locale to en

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  i18n: Ember.inject.service(),
+
+  beforeModel() {
+    this.set('i18n.locale', 'en');
+  },
+});
