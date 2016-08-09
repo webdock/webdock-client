9c640085f517c5d7dd933368740b8521a2029a41
Andre Kupka <kupka@in.tum.de>
2016-08-09 11:35:13 +0200

    add de locale

---
+++ app/locales/de/translations.js
@@ -0,0 +1,70 @@
+export default {
+  // components
+  'container-start-stop-button': {
+    'start': 'Starten',
+    'stop': 'Stoppen',
+  },
+  'docker-container-detail': {
+    'name': 'Name',
+    'status': 'Status',
+    'command': 'Befehl',
+    'id': 'Id',
+    'image': 'Image',
+  },
+  'docker-container-list': {
+    'id': 'Id',
+    'name': 'Name',
+    'status': 'Status',
+    'command': 'Befehl',
+    'image': 'Image',
+  },
+  'docker-image-detail': {
+    'repository': 'Repository',
+    'created': 'Erstellt',
+    'size': 'Größe',
+    'virtual-size': 'Virtuelle Größe',
+    'parent-id': 'Eltern Id',
+    'id': 'Id',
+  },
+  'docker-image-list': {
+    'id': 'Id',
+    'repository': 'Repository',
+    'created': 'Erstellt',
+    'size': 'Größe',
+  },
+  'login-form': {
+    'email': 'E-Mail',
+    'password': 'Passwort',
+    'login': 'Einloggen',
+  },
+  'nav-bar': {
+    'home': 'Home',
+    'containers': 'Container',
+    'images': 'Images',
+    'users': 'Benutzer',
+    'logout': 'Ausloggen',
+  },
+  // routes
+  'containers': {
+    'detail': {
+      'container': 'Container',
+    },
+    'index': {
+      'containers': 'Container',
+    },
+  },
+  'images': {
+    'detail': {
+      'image': 'Image',
+    },
+    'index': {
+      'images': 'Images',
+    },
+  },
+  'users': {
+    'index': {
+      'users': 'Benutzer',
+      'email': 'E-Mail',
+    },
+  },
+};
