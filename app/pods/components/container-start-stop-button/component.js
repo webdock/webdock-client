6d15402bc2fffdedc38fc9aed7906320a28d46e1
Andre Kupka <kupka@in.tum.de>
2016-08-09 10:31:40 +0200

    add toastr notifications for container start/stop button

--- app/pods/components/container-start-stop-button/component.js
+++ app/pods/components/container-start-stop-button/component.js
@@ -2,34 +2,39 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
+  toast: Ember.inject.service(),
 
   dockerContainer: null,
 
-  updateModel(response) {
+  updateContainer(response, changeMessage, keptMessage) {
+    const toast = this.get('toast');
     if (Ember.isEmpty(response)) {
+      toast.info(keptMessage);
       return;
     }
-    console.log(response);
     this.get('store').pushPayload(response);
+    toast.success(changeMessage);
   },
 
   handleError(error) {
-    console.log(error);
+    this.get('toast').error('An error occurred!');
   },
 
-  changeStatus(action) {
+  changeStatus(action, changeMessage, keptMessage) {
     this.get('dockerContainer')[action]()
-      .then(response => this.updateModel(response))
+      .then(response => this.updateContainer(response, changeMessage, keptMessage))
       .catch(error => this.handleError(error));
   },
 
   actions: {
     start() {
-      this.changeStatus('start')
+      const name = this.get('dockerContainer.name');
+      this.changeStatus('start', `Container ${name} started!`, `Container ${name} was already running!`);
     },
 
     stop() {
-      this.changeStatus('stop')
-    }
+      const name = this.get('dockerContainer.name');
+      this.changeStatus('stop', `Container ${name} stopped!`, `Container ${name} was not running!`);
+    },
   }
 });
